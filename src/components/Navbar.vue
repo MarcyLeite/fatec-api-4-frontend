a537d570cc1f53179f215c68f3f3d6828cf0e9b7
Marilia Moraes <marilia.bm01@gmail.com>
2025-03-14 00:14:21 +0000

    feat: cria navbar e footer para o site

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app-bar color="deep-purple-darken-1" :elevation="2">
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+        <v-app-bar-title>
+            <v-col class="d-flex">
+                <v-icon class="mr-3">
+                    <img src="@/assets/logo-terravision.svg" alt="Icon" class="icon-size">
+                </v-icon>
+                <span> Terravision </span>
+            </v-col>
+        </v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+        <v-list>
+            <v-list-item link href="/">
+                <v-list-item-content>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            drawer: false,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.icon-size {
+    width: 64px;
+    height: 64px;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,15 @@
 <template>
   <v-main>
+    <Navbar />
     <router-view />
+    <Footer />
   </v-main>
 
-  <AppFooter />
 </template>
 
 <script lang="ts" setup>
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
+
   //
 </script>
