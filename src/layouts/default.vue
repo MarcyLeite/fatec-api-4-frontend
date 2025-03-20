26689a8ff430f9329c7b627cdd8d5a5c4cfdcffc
Matheus Marciano <39443406+MarcyLeite@users.noreply.github.com>
2025-03-20 20:44:11 -0300

    Revert "feat: cria navbar e footer para o site"

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,11 @@
 <template>
   <v-main>
-    <Navbar />
     <router-view />
-    <Footer />
   </v-main>
 
+  <AppFooter />
 </template>
 
 <script lang="ts" setup>
-import Navbar from '../components/Navbar.vue';
-import Footer from '../components/Footer.vue';
-
   //
 </script>
