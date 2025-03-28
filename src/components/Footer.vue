c35bee68bd75688b6251a1d5e94d9976d39d7abe
Matheus Marciano <mm.leitea@outlook.com>
2025-03-28 13:36:28 +0000

    chore (#5): utilizar script setup em componentes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-footer color="deep-purple-lighten-4 d-flex justify-center">
+    <v-footer color="deep-purple-lighten-4 d-flex justify-center" app>
         <div>
             <v-icon class="mr-3">
                 <img src="@/assets/logo-aldebaram.svg" alt="Icon" class="icon-size">
@@ -9,22 +9,5 @@
     </v-footer>
 </template>
 
-<script>
-export default {
-    name: 'FooterComponent',
-}
+<script setup>
 </script>
-
-<style scoped>
-.v-footer {
-    background-color: #3f51b5;
-    color: white;
-}
-.v-icon img {
-    vertical-align: middle;
-}
-.icon-size {
-    width: 48px;
-    height: 48px;
-}
-</style>
