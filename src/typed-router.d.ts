d7a23bd78f933736f5dd6a4fe138528428970be9
Marilia Moraes <marilia.bm01@gmail.com>
2025-03-29 14:32:49 +0000

    fix #3: conflito de paginas

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -20,6 +20,6 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/fazenda/': RouteRecordInfo<'/fazenda/', '/fazenda', Record<never, never>, Record<never, never>>,
-    '/talhao/': RouteRecordInfo<'/talhao/', '/talhao', Record<never, never>, Record<never, never>>,
+    '/talhao/listar': RouteRecordInfo<'/talhao/listar', '/talhao/listar', Record<never, never>, Record<never, never>>,
   }
 }
