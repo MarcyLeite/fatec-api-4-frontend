94939e55c71d7320d901f21aed13f1da63e26262
Matheus Marciano <mm.leitea@outlook.com>
2025-03-30 14:23:46 +0000

    chore: Mudar nome e icone da aplicação

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-      <HelloWorld />
+      <v-responsive max-width="960" class="mx-auto">
+            <div>
+                  <span class="text-h6">Futura pagina de login</span>
+            </div>
+      </v-responsive>
 </template>
 
 <script lang="ts" setup>
