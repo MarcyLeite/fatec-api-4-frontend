4c43903dc7da31ccfb2988cd460aa6148c373a73
Matheus Marciano <mm.leitea@outlook.com>
2025-05-25 01:57:12 +0000

    fix(#35): Arrumar dashboard typo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-app-bar
-    class="px-2"
-    color="deep-purple-darken-1"
-    :elevation="2"
-  >
+  <v-app-bar class="px-2" color="deep-purple-darken-1" :elevation="2">
     <template #prepend>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
     </template>
@@ -14,40 +10,27 @@
             src="@/assets/logo-terravision.svg"
             alt="Icon"
             class="icon-size"
-          >
+          />
         </v-icon>
         <span> Terravision </span>
       </v-col>
     </v-app-bar-title>
     <template #append>
       <div v-if="store.token === ''">
-        <v-btn
-          icon="mdi-login"
-          to="/login"
-        />
+        <v-btn icon="mdi-login" to="/login" />
       </div>
-      <div
-        v-else
-        class="d-flex ga-2 align-center"
-      > 
-        <div>{{ store.name }} </div> <v-divider vertical /> <div> {{ store.role }} </div>
-        <v-btn
-          icon="mdi-logout"
-          @click="store.logout()"
-        />
+      <div v-else class="d-flex ga-2 align-center">
+        <div>{{ store.name }}</div>
+        <v-divider vertical />
+        <div>{{ store.role }}</div>
+        <v-btn icon="mdi-logout" @click="store.logout()" />
       </div>
     </template>
   </v-app-bar>
 
-  <v-navigation-drawer
-    v-model="drawer"
-    app
-  >
+  <v-navigation-drawer v-model="drawer" app>
     <v-list>
-      <v-list-item
-        link
-        href="/"
-      >
+      <v-list-item link href="/">
         <v-list-item-content>
           <v-list-item-title>Home</v-list-item-title>
         </v-list-item-content>
@@ -97,13 +80,13 @@
           <v-list-item-title>Resultado</v-list-item-title>
         </v-list-content>
       </v-list-item>
-            <v-list-item
+      <v-list-item
         v-if="['Administrador'].includes(store.role ?? '')"
         link
         href="/dashboard"
       >
         <v-list-content>
-          <v-list-item-title>dashboard</v-list-item-title>
+          <v-list-item-title>Dashboard</v-list-item-title>
         </v-list-content>
       </v-list-item>
     </v-list>
@@ -111,13 +94,11 @@
 </template>
 
 <script lang="ts" setup>
-import { useAppStore } from '@/stores/app';
+import { useAppStore } from "@/stores/app";
 
+const drawer = ref(false);
 
-const drawer = ref(false)
-
-const store = useAppStore()
-
+const store = useAppStore();
 </script>
 
 <style scoped>
